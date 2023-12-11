6d7876e69317d7db05230f3c68af69e1c7b2aefb
ScanziFilippo <101709401+ScanziFilippo@users.noreply.github.com>
2023-12-11 12:31:38 +0000

    Add SQL

---
+++ SQL/parcheggi.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `parcheggi` (
+    id int(11) NOT NULL PRIMARY KEY,
+    stato varchar(255),
+    aereopoto_icao varchar(255)
+)
---
+++ SQL/voli.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `voli` (
+    id int(11) NOT NULL PRIMARY KEY,
+    partenza varchar(255),
+    destinazione varchar(255),
+    data_partenza varchar(255),
+    data_arrivo varchar(255),
+    aereo_immatricolazione varchar(255)
+)
