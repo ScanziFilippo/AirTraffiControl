98c20f553fbbaf7ba2eebb4ac1749eca5ed20717
ScanziFilippo <101709401+ScanziFilippo@users.noreply.github.com>
2023-12-14 09:28:47 +0000

    Update (timestamp)

--- SQL/voli.sql
+++ SQL/voli.sql
@@ -2,7 +2,7 @@ CREATE TABLE `voli` (
     id int(11) NOT NULL PRIMARY KEY,
     partenza varchar(255),
     destinazione varchar(255),
-    data_partenza varchar(255),
-    data_arrivo varchar(255),
+    data_partenza timestamp,
+    data_arrivo timestamp,
     aereo_immatricolazione varchar(255)
 )
