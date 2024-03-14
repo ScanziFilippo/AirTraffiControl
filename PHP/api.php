3dfb34831eeeee85be47e893ca8f4f26d98f6eb7
ScanziFilippo <101709401+ScanziFilippo@users.noreply.github.com>
2024-03-14 10:53:43 +0000

    Added API

---
+++ PHP/api.php
@@ -0,0 +1,50 @@
+<?php
+    $connessione = new mysqli("localhost","root","","progetto");
+    if($connessione->connect_errno)
+    {
+        echo("Connessione fallita: ".$connessione->connect_error.".");
+        exit();
+    }
+    /*if(isset($_GET['tabella'])){
+        $tabella = $_GET['tabella'];*/
+    if(isset($_REQUEST['tabella'])){
+        $tabella = $_REQUEST['tabella'];
+        if(isset($_REQUEST['id'])){
+            $id = $_REQUEST['id'];
+            if($tabella == 'compagnie' || $tabella == 'piste' || $tabella == 'parcheggi' || $tabella == 'voli')
+                $query = "SELECT * FROM ".$tabella." WHERE id = ".$id;
+            else if($tabella == 'aerei')
+                $query = "SELECT * FROM ".$tabella." WHERE immatricolazione = '".$id."'";
+            else if($tabella == 'aeroporti')
+                $query = "SELECT * FROM ".$tabella." WHERE icao = '".$id."'";
+            else if($tabella == 'controllori')
+                $query = "SELECT * FROM ".$tabella." WHERE codice_fiscale = '".$id."'";
+        }else{
+            $query = "SELECT * FROM ".$tabella;
+        }
+        $risultato = $connessione->query($query);
+        header('Content-Type: application/json');
+        echo json_encode($risultato->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);    
+    }else{
+        echo("
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Api</title>
+                <link rel='stylesheet' href='../CSS/index.css'>
+            </head>
+            <body>
+                <div style='padding-left: 20px; padding-top:10px;'>
+                    <h1>Api</h1>
+                    <h2><a href='api.php/aerei'>Aerei</a></h2>
+                    <h2><a href='api.php/aeroporti'>Aeroporti</a></h2>
+                    <h2><a href='api.php/compagnie'>Compagnie</a></h2>
+                    <h2><a href='api.php/controllori'>Controllori</a></h2>
+                    <h2><a href='api.php/voli'>Voli</a></h2>
+                    <h2><a href='api.php/parcheggi'>Parcheggi</a></h2>
+                    <h2><a href='api.php/piste'>Piste</a></h2>
+                </div>
+            </body>
+        </html>");
+    }
+?>
