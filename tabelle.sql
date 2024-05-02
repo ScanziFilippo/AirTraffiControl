6da177f0084bd2a9f6f8d7f488d6d34c7daa405d
ScanziFilippo <101709401+ScanziFilippo@users.noreply.github.com>
2024-05-02 09:28:58 +0000

    Update readme

--- tabelle.sql
+++ tabelle.sql
@@ -6,13 +6,8 @@ CREATE TABLE `aerei` (
     immatricolazione varchar(255) NOT NULL,
     modello varchar(255),
     compagnia varchar(255),
-    foto_aereo varchar(255),
-    foto_compagnia varchar(255),
-    posizione varchar(255),
+    luogo varchar(255),
     stato varchar(255),
-    pista_id int(11),
-    parcheggio_id int(11),
-    aeroporto_id varchar(255)
 );
 CREATE TABLE `aeroporti` (
     id int(11) NOT NULL auto_increment PRIMARY KEY,
