61f9f5d933c85467019f7c67aa68144e62a4ce3e
ScanziFilippo <101709401+ScanziFilippo@users.noreply.github.com>
2024-05-20 11:19:57 +0000

    Style Update

--- PHP/Voli/visualizza.php
+++ PHP/Voli/visualizza.php
@@ -12,29 +12,34 @@
     <div style='padding-left: 20px; padding-top:10px;'>
         <h1>Visualizza voli</h1>
         <table>
-            <tr>
-                <th>Codice</th>
-                <th>Partenza</th>
-                <th>Destinazione</th>
-                <th>Orario partenza</th>
-                <th>Orario arrivo</th>
-            </tr>
-            <?php
-                $query = "SELECT * FROM voli";
-                $risultato = $connessione->query($query);
-                while($riga = $risultato->fetch_assoc()){
-                    echo "<tr>";
-                    echo "<td>".$riga["id"]."</td>";
-                    echo "<td>".$connessione->query("SELECT nome FROM luoghi WHERE id='".$riga["partenza"]."'")->fetch_assoc()["nome"]."</td>";
-                    echo "<td>".$connessione->query("SELECT nome FROM luoghi WHERE id='".$riga["destinazione"]."'")->fetch_assoc()["nome"]."</td>";
-                    echo "<td>".$riga["data_partenza"]."</td>";
-                    echo "<td>".$riga["data_arrivo"]."</td>";
-                    echo "</tr>";
-                }
-            ?>
-        </table>
-    </div>
-        <div style='padding-left: 20px; padding-top:10px;'>
-            <h2><a href='../'>Home</a></h2>
-        </div>
-    </body>
+        <style>
+    td, th {
+        padding: 10px;
+    }
+</style>
+<tr>
+    <th>Codice</th>
+    <th>Partenza</th>
+    <th>Destinazione</th>
+    <th>Orario partenza</th>
+    <th>Orario arrivo</th>
+</tr>
+<?php
+    $query = "SELECT * FROM voli";
+    $risultato = $connessione->query($query);
+    while($riga = $risultato->fetch_assoc()){
+        echo "<tr>";
+        echo "<td>".$riga["id"]."</td>";
+        echo "<td>".$connessione->query("SELECT icao FROM aeroporti WHERE id='".$riga["partenza"]."'")->fetch_assoc()["icao"]."</td>";
+        echo "<td>".$connessione->query("SELECT icao FROM aeroporti WHERE id='".$riga["destinazione"]."'")->fetch_assoc()["icao"]."</td>";
+        echo "<td>".$riga["data_partenza"]."</td>";
+        echo "<td>".$riga["data_arrivo"]."</td>";
+        echo "</tr>";
+    }
+?>
+</table>
+</div>
+<div style='padding-left: 20px; padding-top:10px;'>
+    <h2><a href='../'>Home</a></h2>
+</div>
+</body>
